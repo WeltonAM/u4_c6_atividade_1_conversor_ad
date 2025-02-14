cdb887fec1cf16b2e8fcf1cfa6aea4f73b816325
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2025-02-14 10:08:35 -0300

    Refatorando

--- ConversorAD.c
+++ ConversorAD.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#include "hardware/pwm.h"
 #include "hardware/i2c.h"
 #include "lib/ssd1306.h"
 #include "lib/font.h"
+#include "pico/bootrom.h"
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -14,32 +17,55 @@
 #define JOYSTICK_PB 22
 #define Botao_A 5
 #define Botao_B 6
-#include "pico/bootrom.h"
+#define LED_R 11
+#define LED_G 12
+#define LED_B 13
 
 ssd1306_t ssd;
+bool led_pwm_ativo = true;
+bool led_verde_estado = false;
+int borda_estilo = 0;
 
-void gpio_irq_handler(uint gpio, uint32_t events)
+uint pwm_init_gpio(uint gpio, uint wrap)
 {
+  gpio_set_function(gpio, GPIO_FUNC_PWM);
+  uint slice_num = pwm_gpio_to_slice_num(gpio);
+  pwm_set_wrap(slice_num, wrap);
+  pwm_set_enabled(slice_num, true);
+  return slice_num;
+}
+
+void joystick_button_irq(uint gpio, uint32_t events)
+{
+  led_verde_estado = !led_verde_estado;
+  borda_estilo = (borda_estilo + 1) % 3;
+}
+
+void botao_a_irq(uint gpio, uint32_t events)
+{
+  led_pwm_ativo = !led_pwm_ativo;
+}
+
+void botao_b_irq(uint gpio, uint32_t events)
+{
+  ssd1306_fill(&ssd, false);
+  ssd1306_send_data(&ssd);
   reset_usb_boot(0, 0);
 }
 
 int main()
 {
-  gpio_init(Botao_B);
-  gpio_set_dir(Botao_B, GPIO_IN);
-  gpio_pull_up(Botao_B);
-  gpio_set_irq_enabled_with_callback(Botao_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+  stdio_init_all();
+  adc_init();
+  adc_gpio_init(JOYSTICK_X_PIN);
+  adc_gpio_init(JOYSTICK_Y_PIN);
 
-  gpio_init(JOYSTICK_PB);
-  gpio_set_dir(JOYSTICK_PB, GPIO_IN);
-  gpio_pull_up(JOYSTICK_PB);
-
-  gpio_init(Botao_A);
-  gpio_set_dir(Botao_A, GPIO_IN);
-  gpio_pull_up(Botao_A);
+  uint pwm_wrap = 4095;
+  pwm_init_gpio(LED_R, pwm_wrap);
+  pwm_init_gpio(LED_G, pwm_wrap);
+  pwm_init_gpio(LED_B, pwm_wrap);
 
   i2c_init(I2C_PORT, 400 * 1000);
-
   gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
   gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
   gpio_pull_up(I2C_SDA);
@@ -49,43 +75,52 @@ int main()
   ssd1306_config(&ssd);
   ssd1306_send_data(&ssd);
 
-  ssd1306_fill(&ssd, false);
-  ssd1306_send_data(&ssd);
+  gpio_init(JOYSTICK_PB);
+  gpio_set_dir(JOYSTICK_PB, GPIO_IN);
+  gpio_pull_up(JOYSTICK_PB);
+  gpio_set_irq_enabled_with_callback(JOYSTICK_PB, GPIO_IRQ_EDGE_FALL, true, &joystick_button_irq);
 
-  adc_init();
-  adc_gpio_init(JOYSTICK_X_PIN);
-  adc_gpio_init(JOYSTICK_Y_PIN);
+  gpio_init(Botao_A);
+  gpio_set_dir(Botao_A, GPIO_IN);
+  gpio_pull_up(Botao_A);
+  gpio_set_irq_enabled_with_callback(Botao_A, GPIO_IRQ_EDGE_FALL, true, &botao_a_irq);
 
-  uint16_t adc_value_x;
-  uint16_t adc_value_y;
-  char str_x[5];
-  char str_y[5];
+  gpio_init(Botao_B);
+  gpio_set_dir(Botao_B, GPIO_IN);
+  gpio_pull_up(Botao_B);
+  gpio_set_irq_enabled_with_callback(Botao_B, GPIO_IRQ_EDGE_FALL, true, &botao_b_irq);
 
-  bool cor = true;
   while (true)
   {
     adc_select_input(0);
-    adc_value_x = adc_read();
+    uint16_t adc_x = adc_read();
     adc_select_input(1);
-    adc_value_y = adc_read();
-    sprintf(str_x, "%d", adc_value_x);
-    sprintf(str_y, "%d", adc_value_y);
+    uint16_t adc_y = adc_read();
 
-    ssd1306_fill(&ssd, !cor);
-    ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);
-    ssd1306_line(&ssd, 3, 25, 123, 25, cor);
-    ssd1306_line(&ssd, 3, 37, 123, 37, cor);
-    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6);
-    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);
-    ssd1306_draw_string(&ssd, "ADC   JOYSTICK", 10, 28);
-    ssd1306_draw_string(&ssd, "X    Y    PB", 20, 41);
-    ssd1306_line(&ssd, 44, 37, 44, 60, cor);
-    ssd1306_draw_string(&ssd, str_x, 8, 52);
-    ssd1306_line(&ssd, 84, 37, 84, 60, cor);
-    ssd1306_draw_string(&ssd, str_y, 49, 52);
-    ssd1306_rect(&ssd, 52, 90, 8, 8, cor, !gpio_get(JOYSTICK_PB));
-    ssd1306_rect(&ssd, 52, 102, 8, 8, cor, !gpio_get(Botao_A));
-    ssd1306_rect(&ssd, 52, 114, 8, 8, cor, !cor);
+    if (led_pwm_ativo)
+    {
+      pwm_set_gpio_level(LED_R, abs((int)adc_x - 2048) * 255 / 2048);
+      pwm_set_gpio_level(LED_B, abs((int)adc_y - 2048) * 255 / 2048);
+    }
+    else
+    {
+      pwm_set_gpio_level(LED_R, 0);
+      pwm_set_gpio_level(LED_B, 0);
+    }
+
+    pwm_set_gpio_level(LED_G, led_verde_estado ? pwm_wrap : 0);
+
+    ssd1306_fill(&ssd, false);
+    if (borda_estilo == 1)
+    {
+      ssd1306_rect(&ssd, 0, 0, WIDTH, HEIGHT, true, false);
+    }
+    else if (borda_estilo == 2)
+    {
+      ssd1306_rect(&ssd, 2, 2, WIDTH - 4, HEIGHT - 4, true, false);
+    }
+
+    ssd1306_rect(&ssd, 60 + (adc_x * 128 / 4095) - 4, 32 + (adc_y * 64 / 4095) - 4, 8, 8, true, true);
     ssd1306_send_data(&ssd);
 
     sleep_ms(100);
